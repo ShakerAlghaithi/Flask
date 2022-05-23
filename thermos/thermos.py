4ac80d55a404126ca6449f02c050cff9e7fb26eb
Shaker Alghaithi <ghaithishake@gmail.com>
2022-05-23 16:37:56 -0400

    rewrite the add() to use the forms file

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,12 +1,12 @@
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for
-from logging import DEBUG
+from flask import Flask, render_template, request, redirect, url_for, flash
+from forms import bookmarkForm
 
 app = Flask(__name__)
-app.logger.setLevel(DEBUG)
+
 # shouldn't use global list to store data, but i am using it for praticing at the moment.
 bookmarks= []
-
+app.config['SECRET_KEY']= '\x85\x8a\x15\x8f\x8a\xe1\x0e\xca\x1b\\\xf1\xb7\xb8\xc7\xb5\x13\x1c\xe8s\x83\xb8\xfa\xe0\xb9'
 def store_bookmark(url):
     bookmarks.append(dict(
         url=url,
@@ -15,20 +15,24 @@ def store_bookmark(url):
 
     ))
 
+def new_bookmarks(num):
+    return sorted(bookmarks, key=lambda bm: bm['date'], reverse=True)[:num]
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return render_template('index.html')
+    return render_template('index.html', new_bookmarks=new_bookmarks(5))
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():      # no validation yet here
-    if request.method == "POST":
-        url = request.form['url']
-        store_bookmark(url)
-        app.logger.debug('stored url: ' + url)
+    form = bookmarkForm()
+    if form.validate_on_submit():
+        url = form.url.data
+        description = form.description.data
+        store_bookmark(url, description)
+        flash("Stored bookmark: '{}'".format(description))
         return redirect(url_for('index'))
-    return render_template("add.html")
+    return render_template('add.html', form=form)
 
 @app.errorhandler(404)
 def page_not_found(e):
