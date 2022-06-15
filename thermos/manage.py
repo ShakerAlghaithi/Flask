f0c523f8585daf80acbebe52fe22f8c62d890440
Shaker Alghaithi <ghaithishake@gmail.com>
2022-06-15 14:51:09 -0400

    creating relationship between db tables and adding ignore file

--- thermos/manage.py
+++ thermos/manage.py
@@ -2,13 +2,17 @@
 
 from app_thermos import app, db
 from flask_script import Manager, prompt_bool
-
+from models import User 
 manager = Manager(app)
 
 @manager.command
 def initdb():
     db.create_all()
+    db.session.add(User(username='Abdul', email='abdul@alghaithi.com'))
+    db.session.add(User(username='Manar', email='manar@alghaithi.com'))
+    db.session.commit()
     print('Initialized the database')
+
 
 @manager.command
 def dropdb():
